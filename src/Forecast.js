34285730fd88b1c8cd050341aa43825f62e46425
Adriel Klein <adrielmklein@gmail.com>
2019-06-23 13:24:19 -0700

    make forecast component work with dummy data

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
+const predictions = [{
+    "dt": 1561323600,
+    "main": {
+        "temp": 297.6,
+        "temp_min": 297.6,
+        "temp_max": 298.152,
+        "pressure": 1007.73,
+        "sea_level": 1007.73,
+        "grnd_level": 970.38,
+        "humidity": 84,
+        "temp_kf": -0.55
+    },
+    "weather": [
+        {
+            "id": 804,
+            "main": "Clouds",
+            "description": "overcast clouds",
+            "icon": "04d"
+        }
+    ],
+    "dt_txt": "2019-06-23 21:00:00"
+}]
+
+const city = {
+    "id": 4409896,
+    "name": "Springfield",
+    "coord": {
+        "lat": 37.2167,
+        "lon": -93.2921
+    },
+    "country": "US",
+    "population": 159498,
+    "timezone": -18000
+};
 function Forecast() {
     return (
         <div style={{
                 display: 'grid',
         }}>
-            <h2>Showing results for Boston (Coordinates 34LONG 343 LAT)</h2>
+            <h2>Showing results for {city.name} (Coordinates {city.coord.lat} Lat {city.coord.lon} Long)</h2>
             <div style={{
                 display: 'grid',
                 gridTemplateColumns: '100px 100px 100px',
@@ -15,16 +49,16 @@ function Forecast() {
                 <div><b>temperature</b></div>
                 <div><b>description</b></div>
             </div>
-
-            <div style={{
+            {predictions.map(prediction => <div style={{
                 display: 'grid',
                 gridTemplateColumns: '100px 100px 100px',
                 gridGap: '10px',
             }}>
-                <div>3PM</div>
-                <div>34 defrees F</div>
-                <div>cloudy</div>
-            </div>
+                <div>{prediction.dt_txt}</div>
+                <div>{prediction.main.temp}</div>
+                <div>{prediction.weather[0].main}</div>
+
+            </div>)}
         </div>
     );
 }
