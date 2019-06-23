11dff51ff967f8004d925420a7853441b42424ce
Adriel Klein <adrielmklein@gmail.com>
2019-06-23 14:07:03 -0700

    get response from api and do error handling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Forecast from "./Forecast";
+import axios from 'axios';
+import _ from 'lodash';
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 function App() {
+    const [query, setQuery] = useState();
+    const [predictions, setPredictions] = useState();
+    const [city, setCity] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSearchForCity = async () => {
+        setIsLoading(true);
+        try {
+            const data = await axios.get(API_URL, {
+                params: {
+                    q: `${query},us`,
+                    APPID: 'cea8a222fb5dfc35ac3e87b2e9ffaae5',
+                }
+            });
+            setPredictions(data.data.list);
+            setCity(data.data.city);
+        } catch (error) {
+            const defaultMessage = 'An error occured while searching for the forecast';
+            alert(_.get(error, 'response.data.message', defaultMessage));
+        }
+        setIsLoading(false);
+    }
     return (
         <div style={{display: 'grid', padding: '20px'}}>
             <h1>Weather</h1>
             <div>
                 <label>Search By City</label>
-                <input/>
-                <button>Search</button>
+                <input type="text" value={query} onChange={(event) => setQuery(event.target.value)}/>
+                <button onClick={handleSearchForCity}>Search</button>
             </div>
-            <div>
-                <label>Search By Coordinates</label>
-                <input/>
-                <button>Search</button>
-            </div>
-            <Forecast />
+            {/*<div>*/}
+            {/*    <label>Search By Coordinates</label>*/}
+            {/*    <input/>*/}
+            {/*    <button>Search</button>*/}
+            {/*</div>*/}
+            <Forecast isLoading={isLoading} predictions={predictions} city={city}/>
         </div>
     );
 }
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,59 +1,31 @@
 import React from 'react';
 
-const predictions = [{
-    "dt": 1561323600,
-    "main": {
-        "temp": 297.6,
-        "temp_min": 297.6,
-        "temp_max": 298.152,
-        "pressure": 1007.73,
-        "sea_level": 1007.73,
-        "grnd_level": 970.38,
-        "humidity": 84,
-        "temp_kf": -0.55
-    },
-    "weather": [
-        {
-            "id": 804,
-            "main": "Clouds",
-            "description": "overcast clouds",
-            "icon": "04d"
-        }
-    ],
-    "dt_txt": "2019-06-23 21:00:00"
-}]
-
-const city = {
-    "id": 4409896,
-    "name": "Springfield",
-    "coord": {
-        "lat": 37.2167,
-        "lon": -93.2921
-    },
-    "country": "US",
-    "population": 159498,
-    "timezone": -18000
+const gridStyle = {
+    display: 'grid',
+    gridTemplateColumns: '200px 100px 100px',
+    gridGap: '20px',
 };
-function Forecast() {
+
+function Forecast({predictions, city, isLoading}) {
+    if (!predictions || !city){
+        return 'No results to display'
+    }
+    if (isLoading){
+        return 'Loading...'
+    }
     return (
         <div style={{
-                display: 'grid',
+            display: 'grid',
         }}>
             <h2>Showing results for {city.name} (Coordinates {city.coord.lat} Lat {city.coord.lon} Long)</h2>
-            <div style={{
-                display: 'grid',
-                gridTemplateColumns: '100px 100px 100px',
-                gridGap: '10px',
-            }}>
+            <div style={gridStyle}>
                 <div><b>time</b></div>
                 <div><b>temperature</b></div>
                 <div><b>description</b></div>
             </div>
-            {predictions.map(prediction => <div style={{
-                display: 'grid',
-                gridTemplateColumns: '100px 100px 100px',
-                gridGap: '10px',
-            }}>
+            {predictions.map(prediction => <div
+                key={prediction.dt_txt}
+                style={gridStyle}>
                 <div>{prediction.dt_txt}</div>
                 <div>{prediction.main.temp}</div>
                 <div>{prediction.weather[0].main}</div>
